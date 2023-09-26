282e800515d96802bd98154b521dc7e0fca20589
JordanSanchez06 <jordansanchez100603@gmail.com>
2023-09-26 13:14:55 -0400

    Made the -p option match test cases

--- machine_main.c
+++ machine_main.c
@@ -15,17 +15,49 @@ static union mem_u {
     bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
 } memory; //https://webcourses.ucf.edu/courses/1443631/discussion_topics/7460061
 
+void printTrace();
+
 int main(int argc , char **argv){
     if(strcmp(argv[1],"-p") == 0) {
 
         BOFFILE bf = bof_read_open(argv[2]);
-
         BOFHeader bh = bof_read_header(bf);
 
-        printf("%s\n", instruction_assembly_form(instruction_read(bf)));
-        printf("%s\n", instruction_assembly_form(instruction_read(bf)));
-        printf("%s\n", instruction_assembly_form(instruction_read(bf)));
+        //print instructions
+        printf("Addr Instruction\n");
+        int pc = 0;
+        for(int i = 0; i < (bh.text_length / BYTES_PER_WORD); i++ ) {
+            printf("\t%d %s\n", pc, instruction_assembly_form(instruction_read(bf)));
+            pc += 4;
+        }
+
+        //print initial data values.
+        int address = bh.data_start_address;
+        int wordCount = bh.data_length / BYTES_PER_WORD;
+        if(wordCount == 0)
+            printf("\t %d: %d ...\n", address,  (bh.data_length / BYTES_PER_WORD));
+        for(int i = 0; i < (wordCount); i++ ) {
+            printf("\t    %d: %d", address , bof_read_word(bf)); //spacing to match test case
+        }
+        printf("\n");
 
         bof_close(bf);
+
+        return 0;
     }
+
+
 }
+
+ /*void printTrace(int pc){
+     printf("\tPC: %d", );
+     GPR[$0 ]: 0   	GPR[$at]: 0   	GPR[$v0]: 0   	GPR[$v1]: 0   	GPR[$a0]: 0   	GPR[$a1]: 0
+     GPR[$a2]: 0   	GPR[$a3]: 0   	GPR[$t0]: 0   	GPR[$t1]: 0   	GPR[$t2]: 0   	GPR[$t3]: 0
+     GPR[$t4]: 0   	GPR[$t5]: 0   	GPR[$t6]: 0   	GPR[$t7]: 0   	GPR[$s0]: 0   	GPR[$s1]: 0
+     GPR[$s2]: 0   	GPR[$s3]: 0   	GPR[$s4]: 0   	GPR[$s5]: 0   	GPR[$s6]: 0   	GPR[$s7]: 0
+     GPR[$t8]: 0   	GPR[$t9]: 0   	GPR[$k0]: 0   	GPR[$k1]: 0   	GPR[$gp]: 1024	GPR[$sp]: 4096
+     GPR[$fp]: 4096	GPR[$ra]: 0
+     1024: 0	...
+     4096: 0	...
+     ==> addr:    0 STRA
+ }*/
