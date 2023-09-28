1ad4f452005628f221fa53020ee3fd006b1f4d25
SirPlepingham <146028531+SirPlepingham@users.noreply.github.com>
2023-09-28 12:40:35 -0400

    Update machine_main.c

--- machine_main.c
+++ machine_main.c
@@ -1,32 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "machine_types.h"
-#include "instruction.h"
-#include "bof.h"
-#include "machine.h"
-
-// a size for the memory (2^16 bytes = 64K)
-#define MEMORY_SIZE_IN_BYTES (65536 - BYTES_PER_WORD)
-#define MEMORY_SIZE_IN_WORDS (MEMORY_SIZE_IN_BYTES / BYTES_PER_WORD)
-
-static union mem_u {
-    byte_type bytes[MEMORY_SIZE_IN_BYTES];
-    word_type words[MEMORY_SIZE_IN_WORDS];
-    bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
-} memory; //https://webcourses.ucf.edu/courses/1443631/discussion_topics/7460061
-
-//data for each register //extern so we can use it in machine.c where our ADD, SUB, etc. functions will be.
-int REGISTERS[NUM_REGISTERS];
-//use regname_get(index) for name of register
-
-//program counter is needed for some immediate instructions
-int pc;
-
-void printTrace(int pc, BOFHeader bh,  bin_instr_t instruction, int words[]);
-void doRegisterInstruction(bin_instr_t instruction);
-void doImmediateInstruction(bin_instr_t instruction, address_type pc);
-void doJumpInstruction(bin_instr_t instruction, address_type i, address_type pc);
+#include "machine_main.h;
 
 int main(int argc , char **argv) {
     if(strcmp(argv[1],"-p") == 0) {//for -p option
