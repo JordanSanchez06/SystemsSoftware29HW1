73d7efc7c04236d9c844440e1719998fbc8d9615
SirPlepingham <146028531+SirPlepingham@users.noreply.github.com>
2023-09-29 14:49:41 -0400

    Update machine_main.h

--- machine_main.h
+++ machine_main.h
@@ -9,7 +9,7 @@
 #define MEMORY_SIZE_IN_BYTES (65536 - BYTES_PER_WORD)
 #define MEMORY_SIZE_IN_WORDS (MEMORY_SIZE_IN_BYTES / BYTES_PER_WORD)
 
-static union mem_u {
+extern static union mem_u {
     byte_type bytes[MEMORY_SIZE_IN_BYTES];
     word_type words[MEMORY_SIZE_IN_WORDS];
     bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
